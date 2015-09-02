5d7a3737c6c274e3b409097e778d7caadc5ad5a7
Kirill Chernakov <ya@kirys.ru>
2015-09-02 18:26:50 +0300

    Prepend instead of append

--- lib/sheets.py
+++ lib/sheets.py
@@ -14,26 +14,12 @@ def connect(conf):
     return gspread.authorize(credent)
 
 
-def append_rows(conn, rows, config):
+def prepend_rows(conn, rows, config):
     spreadsheet = conn.open_by_key(config['id'])
     worksheet = spreadsheet.get_worksheet(config['worksheet'])
     row_index = find_empty_row(worksheet)
     for j, row in enumerate(rows, start=row_index):
+        new_row = []
         for i, cell in enumerate(row):
-            worksheet.update_cell(j, i + 1, cell[1])
-
-def find_empty_row(worksheet):
-    row_index = 1
-    while True:
-        row = worksheet.row_values(row_index)
-        if is_empty_row(row):
-            return row_index
-        row_index += 1
-
-def is_empty_row(row_values):
-    if not row_values:
-         return True
-    for value in row_values:
-        if value != 'None':
-            return False
-    return True
+            new_row.append(cell[1])
+        worksheet.insert_row(new_row, 2)
