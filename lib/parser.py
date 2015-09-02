3b9531f37e7531192236c498ea0ceb1457905496
Kirill Chernakov <ya@kirys.ru>
2015-09-02 20:41:31 +0300

    Postfix added to parse rules

--- lib/parser.py
+++ lib/parser.py
@@ -10,12 +10,21 @@ logger = logging.getLogger(__name__)
 
 def parse_body(body, rules):
     result = []
-    for name, prefix in rules:
+    for rule in rules:
+        name = rule[0]
+        prefix = rule[1]
+        postfix = None
+        if len(rule) > 2:
+            postfix = rule[2]
+
         regexp = r'^\s*{}\s*([^\n]*)\n'.format(prefix)
         match = re.search(regexp, body, re.M)
 
         if match:
             match = match.group(1).strip()
+            if postfix:
+                match = match[:match.find(postfix)]
+
         else:
             logger.info('{} not found.'.format(name))
 
