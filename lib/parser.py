d56829c5ebb270fff30f0cc8684ca97ed3ea7c6f
homm <homm86@gmail.com>
2015-09-04 14:44:15 +0300

    improve parser

--- lib/parser.py
+++ lib/parser.py
@@ -11,21 +11,14 @@ logger = logging.getLogger(__name__)
 def parse_body(body, rules):
     result = []
     for rule in rules:
-        name = rule[0]
-        prefix = rule[1]
-        postfix = None
-        if len(rule) > 2:
-            postfix = rule[2]
-        if postfix:
-            regexp = r'^\s*{}\s*([^{}|\n]*)'.format(prefix, postfix)
-        else:
-            regexp = r'^\s*{}\s*([^\n]*)\n'.format(prefix)
+        name, prefix = rule[0], rule[1]
+        postfix = '\n' if len(rule) == 2 else rule[2]
 
+        regexp = r'^\s*{}\s*(.*?){}'.format(prefix, postfix)
         match = re.search(regexp, body, re.M)
 
         if match:
             match = match.group(1).strip()
-
         else:
             logger.info('{} not found.'.format(name))
 
