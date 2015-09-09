9699d33130ed8e782b62b1efdb82d7a5972e09ed
Kirill Chernakov <ya@kirys.ru>
2015-09-09 04:03:09 +0300

    Protect from empty cells

--- lib/sheets.py
+++ lib/sheets.py
@@ -17,24 +17,25 @@ def connect(conf):
 def prepend_rows(conn, rows, config):
     spreadsheet = conn.open_by_key(config['id'])
     worksheet = spreadsheet.get_worksheet(config['worksheet'])
+    headers = rows[0]
 
-    col_count = len(rows[0])
-    # Remove unnecessary cols values
-    all_values = [x[:col_count] for x in worksheet.get_all_values()]
+    col_count = len(headers)
+    all_values = worksheet.get_all_values()
+
+    cols_values = get_cols_values(all_values, col_count)
 
     new_values = [[cell[1] for cell in row] for row in rows]
 
     # Getting index of cells with ids
-    col_names = [cell[0] for cell in rows[0]]
+    col_names = [cell[0] for cell in headers]
     id_index = col_names.index('id')
 
-    new_values = sorted(new_values, key=lambda x: int(float(x[0])), reverse=True)
-    print(new_values)
+    new_values = sorted(new_values, key=lambda x: int(float(x[id_index])), reverse=True)
 
     # Inserting new values after headers
-    all_values = [all_values[0]] + new_values + all_values[1:]
+    cols_values = [cols_values[0]] + new_values + cols_values[1:]
 
-    row_count = len(all_values)
+    row_count = len(cols_values)
 
     # Add extra cols or rows if not enough existing
     if worksheet.row_count < row_count:
@@ -46,9 +47,20 @@ def prepend_rows(conn, rows, config):
     cell_list = worksheet.range("A1:" + string.uppercase[col_count - 1] + str(row_count))
 
     # Make flat list of data
-    cell_values = reduce(lambda res, x: res + x, all_values, [])
+    cell_values = reduce(lambda res, x: res + x, cols_values, [])
 
     for i, val in enumerate(cell_values):
         cell_list[i].value = val
 
     worksheet.update_cells(cell_list)
+
+def get_cols_values(all_values, col_count):
+    cols_values = []
+    for row in all_values:
+        cell_values = row[:col_count]
+        diff = col_count - len(cell_values)
+        if diff > 0:
+            for i in range(diff):
+                cell_values.append('')
+        cols_values.append(cell_values)
+    return cols_values
